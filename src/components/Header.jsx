76df34c5adcf8395f37b80ab0f2069813ba0ddc7
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-11 17:13:50 -0600

    pausing on header, switching to fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,57 @@
+import logo from "../images/logo.png";
+import stole from "../images/stole.png";
+
 const Header = () => {
-  const style = {
+  const headerStyle = {
     width: "100%",
-    background: "#333",
-    color: "white",
+    background: "#fff", // White background
+    color: "black",
     padding: "20px",
-    textAlign: "center", // Fixed camelCase issue
+    margin: "20px",
+    textAlign: "center",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    border: "8px solid black", // Thick black border
+  };
+
+  const logoStyle = {
+    height: "100px",
+    marginLeft: "15px",
+  };
+
+  const bannerStyle = {
+    flexGrow: 1,
+    textAlign: "center",
+  };
+
+  const textStyle = {
+    fontFamily: "serif",
+    fontSize: "32px",
+    fontWeight: "bold",
+  };
+
+  const subTextStyle = {
+    fontSize: "16px",
+    fontWeight: "bold",
+  };
+
+  const stoleStyle = {
+    height: "120px",
+    marginRight: "15px",
   };
 
   return (
-    <header style={style}>
-      <h1>Chapel of St. Stephen</h1>
-      <h2>
-        For Deacons and other Servant Leaders who serve the least, the last, and
-        the lost
-      </h2>
+    <header style={headerStyle}>
+      <img src={logo} alt="Chapel Logo" style={logoStyle} />
+      <div style={bannerStyle}>
+        <h1 style={textStyle}>Chapel of St. Stephen</h1>
+        <h2 style={subTextStyle}>
+          For Deacons and other Servant Leaders who serve the least, the last,
+          and the lost
+        </h2>
+      </div>
+      <img src={stole} alt="Stole" style={stoleStyle} />
     </header>
   );
 };
