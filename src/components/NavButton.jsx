8c1088929394db3c982036ec67f2a3513151a84a
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-11 16:15:06 -0600

    polished

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -4,6 +4,17 @@ import { RED, GOLD } from "../colors";
 import { colorChange } from "../scripts/colorChange";
 
 const NavButton = ({ title, description, link, color = RED }) => {
+  const divStyle = {
+    textAlign: "center",
+    flex: "1", // Ensures equal width distribution across the footer
+    display: "flex",
+    flexDirection: "column", // Ensures content stacks downward
+    justifyContent: "flex-start", // Makes sure content grows downward
+    alignItems: "center",
+    maxWidth: "200px", // Prevents excessive stretching
+    minWidth: "150px", // Ensures a reasonable minimum width
+  };
+
   const buttonStyle = {
     padding: "15px 30px",
     margin: "0 15px 5px 15px",
@@ -22,6 +33,15 @@ const NavButton = ({ title, description, link, color = RED }) => {
     maxWidth: "180px", // Prevents stretching but keeps it uniform
   };
 
+  const descriptionStyle = {
+    fontSize: "0.9em",
+    wordWrap: "break-word",
+    overflowWrap: "break-word",
+    textAlign: "center",
+    marginTop: "5px", // Adds spacing between button and text
+    width: "100%", // Prevents text from altering the div width
+  };
+
   const upSize = e => (e.target.style.transform = "scale(1.05)");
   const darkenButton = e =>
     (e.target.style.backgroundColor = colorChange(color, -0.4));
@@ -32,17 +52,7 @@ const NavButton = ({ title, description, link, color = RED }) => {
   };
 
   return (
-    <div
-      style={{
-        textAlign: "center",
-        flex: "1", // Ensures equal width distribution across the footer
-        display: "flex",
-        flexDirection: "column", // Ensures content stacks downward
-        justifyContent: "flex-start", // Makes sure content grows downward
-        alignItems: "center",
-        maxWidth: "200px", // Prevents excessive stretching
-        minWidth: "150px", // Ensures a reasonable minimum width
-      }}>
+    <div style={divStyle}>
       <button
         style={buttonStyle}
         onMouseOver={upSize}
@@ -52,17 +62,7 @@ const NavButton = ({ title, description, link, color = RED }) => {
         onClick={() => (window.location.href = link ?? "#")}>
         {title ?? "Title"}
       </button>
-      <h4
-        style={{
-          fontSize: "0.9em",
-          wordWrap: "break-word",
-          overflowWrap: "break-word",
-          textAlign: "center",
-          marginTop: "5px", // Adds spacing between button and text
-          width: "100%", // Prevents text from altering the div width
-        }}>
-        {description ?? "Description goes here"}
-      </h4>
+      <h4 style={descriptionStyle}>{description ?? "Description goes here"}</h4>
     </div>
   );
 };
