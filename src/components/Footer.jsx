878c30cf64394c1e490662f48ab600ed2f4f45d3
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-11 16:21:43 -0600

    polished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import { NavButton } from "./NavButton";
 
 const Footer = () => {
-  const navFooterStyle = {
+  const footerStyle = {
     width: "100%",
-    color: "white",
     textAlign: "center",
+  };
+
+  const navFooterStyle = {
+    ...footerStyle,
     padding: "10px",
     display: "flex",
     flexDirection: "row",
@@ -15,10 +18,9 @@ const Footer = () => {
   };
 
   const minorFooterStyle = {
-    width: "100%",
+    ...footerStyle,
     background: "#222",
     color: "white",
-    textAlign: "center",
     padding: "5px",
     fontSize: "0.8em",
   };
@@ -32,11 +34,25 @@ const Footer = () => {
             "31-Day Service-Themed Prayer Cycle, Contemplative Music, and Prayer Requests"
           }
         />
-        <NavButton title={"Jim's Writings"} />
-        <NavButton title={"Jim's Hymms"} />
-        <NavButton title={"The Diaconate"} />
-        <NavButton title={"Gift Shop"} />
-        <NavButton title={"Info & Contact"} />
+        <NavButton
+          title={"Jim's Writings"}
+          description={"Sermon Excerpts, Reflections, and Prayers"}
+        />
+        <NavButton
+          title={"Jim's Hymms"}
+          description={"MP3s & Lyrics of Original Songs"}
+        />
+        <NavButton
+          title={"The Diaconate"}
+          description={"Storied and Commenrary on the Life of a Deacon"}
+        />
+        <NavButton title={"Gift Shop"} description={"Link To Etsy"} />
+        <NavButton
+          title={"Info & Contact"}
+          description={
+            "Story behind this site, as well as Info About Jim, and his Contact Info"
+          }
+        />
       </footer>
       <footer style={minorFooterStyle}>Made by Joe P. Shoulak - 2025</footer>
     </>
