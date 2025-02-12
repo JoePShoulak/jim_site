576627f9906b58b084baa5d98fb66980d20a16d3
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-12 02:47:07 -0600

    writings component for imageAside

--- src/pages/Writings.jsx
+++ src/pages/Writings.jsx
@@ -1,33 +1,40 @@
+/* eslint-disable react/prop-types */
 import useTitle from "../hooks/useTitle";
 import lighthouse from "/images/lighthouse.jpeg";
 import elephant_oak from "/images/elephant_oak.jpg";
-const superior_sunset = "/images/superior_sunset_3.JPG";
+const superior_sunset = "/images/superior_sunset.JPG";
 
 import { WATER_BLUE, OAK_GREEN, SUNSET_ORANGE } from "../assets/colors";
+
+const ImageAside = ({ image, color, label }) => (
+  <aside style={{ width: "33%" }}>
+    <div style={{ textAlign: "center" }}>
+      <img src={image} style={{ width: "100%" }} />
+      <h1 style={{ color }}>{label}</h1>
+    </div>
+  </aside>
+);
 
 const Writings = () => {
   useTitle("Writings");
 
   return (
     <main>
-      <aside style={{ width: "33%" }}>
-        <div style={{ textAlign: "center" }}>
-          <img src={lighthouse} style={{ width: "100%" }} />
-          <h1 style={{ color: WATER_BLUE }}>Sermon Excerpts by Scripture</h1>
-        </div>
-      </aside>
-      <section style={{ width: "33%" }}>
-        <div style={{ textAlign: "center" }}>
-          <img src={elephant_oak} style={{ width: "100%" }} />
-          <h1 style={{ color: OAK_GREEN }}>Reflections and Poetry</h1>
-        </div>
-      </section>
-      <aside style={{ width: "33%" }}>
-        <div style={{ textAlign: "center" }}>
-          <img src={superior_sunset} style={{ width: "100%" }} />
-          <h1 style={{ color: SUNSET_ORANGE }}>Humble Prayers</h1>
-        </div>
-      </aside>
+      <ImageAside
+        image={lighthouse}
+        color={WATER_BLUE}
+        label="Sermon Excerpts by Scripture"
+      />
+      <ImageAside
+        image={elephant_oak}
+        color={OAK_GREEN}
+        label="Reflections and Poetry"
+      />
+      <ImageAside
+        image={superior_sunset}
+        color={SUNSET_ORANGE}
+        label="Humble Prayers"
+      />
     </main>
   );
 };
