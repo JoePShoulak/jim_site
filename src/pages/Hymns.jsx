159fb527689c8eb1c396660fae56ba6f09464efc
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-12 01:11:23 -0600

    polish hymms

--- src/pages/Hymns.jsx
+++ src/pages/Hymns.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/prop-types */
 import useTitle from "../hooks/useTitle";
 import { GREEN } from "../assets/colors";
+import songs from "../data/songs.json";
 
 const Hymns = () => {
   useTitle("Hymns");
 
-  const Song = (title, duration, note = "") => (
-    <p style={{ marginBottom: "40px" }}></p>
+  const Song = ({ title, duration, note = "" }) => (
+    <p style={{ marginBottom: "40px" }}>
+      {title} {note && `[${note}]`} ({duration})
+    </p>
   );
 
   return (
@@ -35,27 +39,16 @@ const Hymns = () => {
         }}>
         {/* Left Column */}
         <div style={{ textAlign: "left", width: "45%" }}>
-          {Song("Welcome the Traveler", "3:06")}
-          {Song("Come to Me", "3:27")}
-          {Song("A New Dawn is Alive!", "3:54")}
-          {Song("Mrs. Schrader’s Garden", "3:59")}
-          {Song("In Your Name", "3:11")}
-          {Song("Faithful Servant", "4:11")}
-          {Song("Say the Word!", "2:55")}
-          {Song("Jerusalem", "3:35")}
-          {Song("Sister Mary Boniface", "4:22")}
+          {songs.leftColumn.map((song, index) => (
+            <Song key={index} {...song} />
+          ))}
         </div>
 
         {/* Right Column */}
         <div style={{ textAlign: "right", width: "45%" }}>
-          {Song("Hardships & Trials", "3:22")}
-          {Song("Psalm 116", "3:07")}
-          {Song("God Gave Me Everything", "4:15")}
-          {Song("Holy Is His Name", "3:01", "The Magnificat")}
-          {Song("For You Are My People", "3:43")}
-          {Song("A Believing Thomas", "3:09")}
-          {Song("Silent Prayer", "4:21", "Lyrics by My Mom")}
-          {Song("Paradise Bay", "4:20")}
+          {songs.rightColumn.map((song, index) => (
+            <Song key={index} {...song} />
+          ))}
         </div>
       </div>
     </div>
