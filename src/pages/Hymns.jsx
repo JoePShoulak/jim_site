8b2e9e6edd0c94ac9cdc745827a64e2ecb3b2efa
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-12 16:15:33 -0600

    hymns css moved to css file

--- src/pages/Hymns.jsx
+++ src/pages/Hymns.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import useTitle from "../hooks/useTitle";
-import { GREEN } from "../assets/colors";
 import songs from "../data/songs.json";
 
 const Song = ({ title, duration, note = "" }) => (
@@ -13,40 +12,18 @@ const Hymns = () => {
   useTitle("Hymns");
 
   return (
-    <main
-      style={{
-        backgroundImage: "url(/images/guitar.png)",
-        backgroundPosition: "center",
-        backgroundSize: "80%", // Scaled down guitar image
-        backgroundRepeat: "no-repeat",
-        width: "1300px",
-        maxWidth: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "relative",
-      }}>
+    <main id="hymns">
       {/* Text Columns */}
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "80%",
-          maxWidth: "900px",
-          color: GREEN,
-          fontSize: "20px",
-          fontFamily: "serif",
-          fontWeight: "bold",
-        }}>
+      <div className="text-columns">
         {/* Left Column */}
-        <div style={{ textAlign: "left", width: "45%" }}>
+        <div className="left-column">
           {songs.leftColumn.map((song, index) => (
             <Song key={index} {...song} />
           ))}
         </div>
 
         {/* Right Column */}
-        <div style={{ textAlign: "right", width: "45%" }}>
+        <div className="right-column">
           {songs.rightColumn.map((song, index) => (
             <Song key={index} {...song} />
           ))}
