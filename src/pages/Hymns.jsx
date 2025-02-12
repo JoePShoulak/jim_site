a60e2ac73861c1099cf99c39f8eae9be316eae95
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-12 01:15:19 -0600

    Fix build issues

--- src/pages/Hymns.jsx
+++ src/pages/Hymns.jsx
@@ -3,14 +3,14 @@ import useTitle from "../hooks/useTitle";
 import { GREEN } from "../assets/colors";
 import songs from "../data/songs.json";
 
+const Song = ({ title, duration, note = "" }) => (
+  <p style={{ marginBottom: "40px" }}>
+    {title} {note && `[${note}]`} ({duration})
+  </p>
+);
+
 const Hymns = () => {
   useTitle("Hymns");
-
-  const Song = ({ title, duration, note = "" }) => (
-    <p style={{ marginBottom: "40px" }}>
-      {title} {note && `[${note}]`} ({duration})
-    </p>
-  );
 
   return (
     <div
