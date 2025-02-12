8577aa63389b5fa9b94239603e3bee3d6d7843c5
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-11 22:16:48 -0600

    polish

--- src/pages/Writings.jsx
+++ src/pages/Writings.jsx
@@ -1,13 +1,14 @@
 const Writings = () => (
   <main>
-    <aside></aside>
+    <aside>
+      <h1>Sermon Excertps by Scripture</h1>
+    </aside>
     <main>
-      <div>
-        <h1>Writings</h1>
-        <p>Writings</p>
-      </div>
+      <h1>Reflections and Poetry</h1>
     </main>
-    <aside></aside>
+    <aside>
+      <h1>Humble Prayers</h1>
+    </aside>
   </main>
 );
 
