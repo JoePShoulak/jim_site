be3812708c9811775ef03895b2559a3e50f96787
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-12 22:17:24 -0600

    fixed alt nav button, bad prop name change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
   <>
     <footer id="navFooter">
       {Object.entries(buttonProps).map(([key, bp]) => (
-        <NavButton key={key} {...bp} color={bp.alt ? GREEN : RED} />
+        <NavButton key={key} {...bp} backgroundColor={bp.alt ? GREEN : RED} />
       ))}
     </footer>
     <footer id="minorFooter">
