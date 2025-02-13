29607f413f76ff53891194f2288e3f157e8163cb
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-12 21:21:28 -0600

    removed superfluities from server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-
-// Manually import 'process' if it's not globally defined
 import { env } from "process";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config();
 
 const app = express();
 const PORT = env.PORT || 5000; // Use 'env' instead of 'process.env'
@@ -20,7 +14,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Handle React Router paths
-app.get("*", (req, res) => {
+app.get("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
