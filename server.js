22e24a627152ea32951b9ec033090c384c1899dc
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-12 21:21:55 -0600

    removed superfluities from server.js

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ import { env } from "process";
 const app = express();
 const PORT = env.PORT || 5000; // Use 'env' instead of 'process.env'
 
-// Fix __dirname for ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// Serve static files from the build folder
 app.use(express.static(path.join(__dirname, "dist")));
 
-// Handle React Router paths
 app.get("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
