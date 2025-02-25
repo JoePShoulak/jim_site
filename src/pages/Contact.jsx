36275d264ba9f75e33a7de7da0b24d588e8be41c
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-25 17:50:38 -0600

    gotta love roboto

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ const CenterSection = () => (
   <section>
     <h2>About</h2>
     <p>
-      Jim Shoulak was born and raised in Wisconsin. He’s been married to Judy
+      Jim Shoulak was born and raised in Wisconsin. He's been married to Judy
       for 40 years, and they have 3 children. They currently live in Minnesota.
       Jim was ordained a deacon in the Episcopal Church on July 29th, 2010. He
       served two parishes and now is privileged to serve as chaplain for his
@@ -15,7 +15,7 @@ const CenterSection = () => (
         “For many years I dreamed of assembling a prayer routine based on the
         Ordination Vows of a Deacon — of course with a focused theme of service.
         With the computer help of my son Joe P. Shoulak, this is the result! I
-        pray that this site will ‘be a service’ to you!”
+        pray that this site will 'be a service' to you!”
       </em>
       <br />
       <br />
