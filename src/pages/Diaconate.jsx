3cfb1bce3edb114defc9b7c5fe19c470aef664f7
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-02-26 17:47:44 -0600

    stole on the diaconate page

--- src/pages/Diaconate.jsx
+++ src/pages/Diaconate.jsx
@@ -1,8 +1,11 @@
+import stole from "/images/original/stole.png";
+
 const LeftAside = () => <aside></aside>;
 
 const CenterSection = () => (
   <section>
     <h2>Diaconate</h2>
+    <img src={stole} alt="a red deacon's stole"></img>
   </section>
 );
 
